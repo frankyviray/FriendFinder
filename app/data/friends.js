e45491270c30f7d8075258d7b6eed05aceebefc8
frank yviray <frankyviray@yahoo.com>
2018-03-22 15:10:21 -0700

    done with friend.js

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friends = [
+    {
+        name: 'Franky',
+        photo: 'https://www.facebook.com/franky.viray',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Hugo',
+        photo: 'https://www.facebook.com/Geetz16',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Matt',
+        photo: 'https://www.facebook.com/matt.hernandez.7906',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Holden',
+        photo: 'https://www.facebook.com/holden.stearns',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Kyle',
+        photo: 'https://www.facebook.com/k.stearns24',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Loni',
+        photo: 'https://www.facebook.com/loni.payne',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Nicole',
+        photo: 'https://www.facebook.com/Nikkibabylove',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Sarrah',
+        photo: 'https://www.facebook.com/sarrah.gallegos',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Shelly',
+        photo: 'https://www.facebook.com/shelley.avraham',
+        scores: [
+            '2',
+            '5',
+            '4',
+            '3',
+            '1',
+            '3',
+            '2',
+            '4',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Pat',
+        photo: 'https://www.facebook.com/pat.mcglynn.98',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    }
+];
+
+module.exports = friends;
